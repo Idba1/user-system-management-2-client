3d71f80f53192eb90be35fad33f32ec89927b489
Idba1 <moniraislamself@gmail.com>
2024-04-24 00:29:59 +0600

    check

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,26 @@ function App() {
     .then(data => setUser(data))
   },[])
 
+  const handleAddUser = event =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email}
+    console.log(user);
+  }
+
   return (
     <>
       <h1>Users Management System</h1>
       <h3>Number of users : {user.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="name" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="add user" />
+      </form>
       <div>
         {
           user.map(user => <p key={user.id}>{user.id} : {user.name} : {user.email} </p>)
